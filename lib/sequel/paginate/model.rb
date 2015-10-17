0d87cf9df50d890b2709a2278f9026d77bea0752
sven199109 <sven199109@gmail.com>
2015-10-17 00:09:33 -0700

    修复引入后无效的问题

--- lib/sequel/paginate/model.rb
+++ lib/sequel/paginate/model.rb
@@ -15,7 +15,11 @@ module Sequel
           dataset_module do
             define_method(:page) do |*args, &block|
               p args
-              page_per = args[1][:page_per]
+              begin
+                page_per = args[1][:page_per]
+              rescue
+                page_per = nil
+              end
               page_no = args.first.to_i > 0 ? args.first : 1
               where.extension(:pagination).paginate(page_no, page_per || base.paginate_per)
             end
