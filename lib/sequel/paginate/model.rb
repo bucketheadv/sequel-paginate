37c7e04762ae4be7664d76aa91dbe0393de7366a
sven199109 <sven199109@gmail.com>
2015-10-17 02:01:42 -0700

    paginate views helper

--- lib/sequel/paginate/model.rb
+++ lib/sequel/paginate/model.rb
@@ -21,7 +21,8 @@ module Sequel
               end
               page_no = args[0].to_i
               page_no = page_no > 0 ? page_no : 1
-              where.extension(:pagination).paginate(page_no, page_per || base.paginate_per)
+              p page_no.class
+              where.extension(:pagination).paginate(page_no.to_i, page_per || base.paginate_per)
             end
           end
         end
