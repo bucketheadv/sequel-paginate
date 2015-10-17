60a87aeff4a5c6f311d94158e14892b46014709e
sven199109 <sven199109@gmail.com>
2015-10-17 01:24:31 -0700

    paginate views helper

--- lib/sequel/paginate.rb
+++ lib/sequel/paginate.rb
@@ -1,5 +1,12 @@
 require "sequel/paginate/version"
 require "sequel/paginate/model"
+require "sequel/paginate/views_helper"
+
+if defined?(Padrino::Application)
+  Padrino::Application.instance_eval {
+    include Sequel::Paginate::ViewsHelper
+  }
+end
 
 module Sequel
   module Paginate
