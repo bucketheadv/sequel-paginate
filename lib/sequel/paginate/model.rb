668950d4c7eb8030d2d9967a6949be738ab6423f
sven199109 <sven199109@gmail.com>
2015-10-16 23:40:40 -0700

    修复引入后无效的问题

--- lib/sequel/paginate/model.rb
+++ lib/sequel/paginate/model.rb
@@ -4,6 +4,8 @@ module Sequel
       def self.extended(base)
         eigen_class = class << base; self; end
         eigen_class.instance_eval do
+          p "HEHEDA"
+          p self
           dataset_module do
             def page(page_no = 1)
               where.extension(:pagination).paginate(page_no, 10)
