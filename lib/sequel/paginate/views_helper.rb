98f7c9456a87985d3e868cf0ef230ea9235b37dc
sven199109 <sven199109@gmail.com>
2015-10-17 01:50:20 -0700

    paginate views helper

--- lib/sequel/paginate/views_helper.rb
+++ lib/sequel/paginate/views_helper.rb
@@ -20,7 +20,7 @@ module Sequel
           html += "<a href='#{path}?page=#{page_no-1}'>Prev</a>"
         end
         page_count.each do |page|
-          html += "<a href='#{path}?page=#{page}'>Prev</a>"
+          html += "<a href='#{path}?page=#{page}'>#{page}</a>"
         end
         if page_no >= page_count.count
           html += "<a>Next</a>"
