3be58bb8702aedd84a64f19c17009b3ffacce932
sven199109 <sven199109@gmail.com>
2015-10-20 08:39:27 -0700

    修改paginate布局

--- lib/sequel/paginate/model.rb
+++ lib/sequel/paginate/model.rb
@@ -8,7 +8,7 @@ module Sequel
         return unless base.ancestors.include?(Sequel::Model)
         class << base
           def paginate_per(count = 20)
-            @@paginate_per ||= count
+            @paginate_per ||= count
           end
         end
 
