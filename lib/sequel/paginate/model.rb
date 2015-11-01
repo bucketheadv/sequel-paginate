d00349453cbc3c564d6534046327e5a3d9424d45
sven199109 <sven199109@gmail.com>
2015-11-01 00:13:46 -0700

    padrino,rails自动加载

--- lib/sequel/paginate/model.rb
+++ lib/sequel/paginate/model.rb
@@ -6,29 +6,30 @@ module Sequel
       end
       def self.extended(base)
         # return unless base.ancestors.include?(Sequel::Model)
-        class << base
-          def paginate_per(count = 20)
-            @paginate_per ||= count
-          end
-        end
+        p base
+        #class << base
+        #  def paginate_per(count = 20)
+        #    @paginate_per ||= count
+        #  end
+        #end
 
-        base.class_eval do
-          dataset_module do
-            define_method(:page) do |*args, &block|
-              begin
-                page_per = args[1][:page_per]
-              rescue
-                page_per = nil
-              end
-              page_no = args[0].to_i
-              page_no = page_no > 0 ? page_no : 1
-              p base
-              p self.class
-              p base.paginate_per
-              where.extension(:pagination).paginate(page_no.to_i, page_per || base.paginate_per)
-            end
-          end
-        end
+        #base.class_eval do
+        #  dataset_module do
+        #    define_method(:page) do |*args, &block|
+        #      begin
+        #        page_per = args[1][:page_per]
+        #      rescue
+        #        page_per = nil
+        #      end
+        #      page_no = args[0].to_i
+        #      page_no = page_no > 0 ? page_no : 1
+        #      p base
+        #      p self.class
+        #      p base.paginate_per
+        #      where.extension(:pagination).paginate(page_no.to_i, page_per || base.paginate_per)
+        #    end
+        #  end
+        #end
       end
     end
   end
