4eece8616be295f98bd175c2379dc163aad16573
sven199109 <sven199109@gmail.com>
2015-10-31 23:48:25 -0700

    padrino,rails自动加载

--- lib/sequel/paginate/model.rb
+++ lib/sequel/paginate/model.rb
@@ -20,6 +20,7 @@ module Sequel
               rescue
                 page_per = nil
               end
+              p page_per
               page_no = args[0].to_i
               page_no = page_no > 0 ? page_no : 1
               where.extension(:pagination).paginate(page_no.to_i, page_per || base.paginate_per)
