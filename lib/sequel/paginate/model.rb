967d0e3fd2de392c0c45daad915e8322e3403dea
sven199109 <sven199109@gmail.com>
2015-11-01 00:18:28 -0700

    padrino,rails自动加载

--- lib/sequel/paginate/model.rb
+++ lib/sequel/paginate/model.rb
@@ -4,40 +4,38 @@ module Sequel
       def self.included(base)
         base.extend(self)
       end
+
       def self.extended(base)
         # return unless base.ancestors.include?(Sequel::Model)
-        p base
-        #class << base
-        #  def paginate_per(count = 20)
-        #    @paginate_per ||= count
-        #  end
-        #end
-
-        #base.class_eval do
-        #  dataset_module do
-        #    define_method(:page) do |*args, &block|
-        #      begin
-        #        page_per = args[1][:page_per]
-        #      rescue
-        #        page_per = nil
-        #      end
-        #      page_no = args[0].to_i
-        #      page_no = page_no > 0 ? page_no : 1
-        #      p base
-        #      p self.class
-        #      p base.paginate_per
-        #      where.extension(:pagination).paginate(page_no.to_i, page_per || base.paginate_per)
-        #    end
-        #  end
-        #end
+        base.class_eval do
+          dataset_module do
+            define_method(:page) do |*args, &block|
+              begin
+                page_per = args[1][:page_per]
+              rescue
+                page_per = nil
+              end
+              page_no = args[0].to_i
+              page_no = page_no > 0 ? page_no : 1
+              p base
+              p self.class
+              p base.paginate_per
+              where.extension(:pagination).paginate(page_no.to_i, page_per || base.paginate_per)
+            end
+          end
+        end
       end
     end
   end
 end
 
 class Sequel::Model
+  include ::Sequel::Paginate::Model
   def self.inherited(base)
-    p base
-    base.send :include, ::Sequel::Paginate::Model
+    class << base
+      def paginate_per(count = 20)
+        @paginate_per ||= count
+      end
+    end
   end
 end
